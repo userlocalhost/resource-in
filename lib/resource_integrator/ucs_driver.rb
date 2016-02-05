bf926076f3cd13bef940366f636e8132486e0aa6
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2016-02-05 18:15:12 +0900

    specify python version associated with each drivers

--- lib/resource_integrator/ucs_driver.rb
+++ lib/resource_integrator/ucs_driver.rb
@@ -3,7 +3,7 @@ require_relative "driver"
 module ResourceIntegrator
   class UCSDriver < Driver
     def initialize
-      @command = File.dirname(__FILE__) + '/driver/ucs/get_servers'
+      @command = "/usr/bin/python2.7 " + File.dirname(__FILE__) + '/driver/ucs/get_servers'
       @cachepath = '/tmp/ucs_servers.cache'
     end
     def get_data
--- lib/resource_integrator/vmware_driver.rb
+++ lib/resource_integrator/vmware_driver.rb
@@ -3,7 +3,7 @@ require_relative "driver"
 module ResourceIntegrator
   class VMwareDriver < Driver
     def initialize
-      @command = File.dirname(__FILE__) + '/driver/vmware/get_servers'
+      @command = "/usr/bin/python3 " + File.dirname(__FILE__) + '/driver/vmware/get_servers'
       @cachepath = '/tmp/vmware_servers.cache'
     end
     def get_data
